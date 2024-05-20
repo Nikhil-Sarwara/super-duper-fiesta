da01039117b6011070132d746c3cb5ccce83155d
Nikhil <117506262+Nikhil-Sarwara@users.noreply.github.com>
2024-05-20 22:54:50 +0530

    add marketing navbar

--- src/components/(Marketing)/NavigationBar/navbar.tsx
+++ src/components/(Marketing)/NavigationBar/navbar.tsx
@@ -1,12 +1,32 @@
-import { Box, Flex, Image, Link } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Link, Text } from "@chakra-ui/react";
+import { FaPerson } from "react-icons/fa6";
+import { GrContact } from "react-icons/gr";
 
 const MarketingNavbar = () => (
-    <Box bg="white" p="4" shadow="md">
+    <Box bg="white" p="4" shadow="md" marginY={"1rem"} rounded="3xl">
         <Flex justify="space-between" align="center">
             <Link href="/">
-                <Image src="/logo.svg" alt="logo" width="200px" />
+                <Flex backgroundColor={"whitesmoke"} padding={"1rem"} rounded={"full"}>
+                    <Image src="https://cdn.iconscout.com/icon/free/png-256/free-resume-1956282-1650445.png" alt="logo" width="2rem" />
+                </Flex>
             </Link>
-            <Link href="/about">About</Link>
+
+            <Heading>
+                Resume Builder
+            </Heading>
+
+            <Flex gap={"1rem"}>
+                <Link href="/about">
+                    <Flex backgroundColor={"whitesmoke"} padding={"1rem"} rounded={"full"} _hover={{ backgroundColor: "gray.100" }}>
+                        <FaPerson />
+                    </Flex>
+                </Link>
+                <Link href="/contact">
+                    <Flex backgroundColor={"whitesmoke"} padding={"1rem"} rounded={"full"} _hover={{ backgroundColor: "gray.100" }}>
+                        <GrContact />
+                    </Flex>
+                </Link>
+            </Flex>
         </Flex>
     </Box>
 );
