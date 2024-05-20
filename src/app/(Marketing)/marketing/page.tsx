23f579ad096774e277d518c4828ee70d6d63dbd8
Nikhil <117506262+Nikhil-Sarwara@users.noreply.github.com>
2024-05-20 22:55:04 +0530

    add react icons and marketing page

--- src/app/(Marketing)/marketing/page.tsx
+++ src/app/(Marketing)/marketing/page.tsx
@@ -1,12 +1,14 @@
 import MarketingNavbar from "@/components/(Marketing)/NavigationBar/navbar";
-import { Flex } from "@chakra-ui/react";
+import { Container, Flex } from "@chakra-ui/react";
 
 const MarketingHome = () => {
     return (
         <main>
-            <MarketingNavbar />
-            <h1>Marketing</h1>
-            <p>This is the marketing home page</p>
+            <Container maxW="container.xl">
+                <MarketingNavbar />
+                <h1>Marketing</h1>
+                <p>This is the marketing home page</p>
+            </Container>
         </main>
     );
 };
